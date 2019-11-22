50bb68a20a8cc23541e7004bcec6ef5d9ff56a8a
Thiago Gomes <thigomes.ti@gmail.com>
2019-11-22 11:49:54 -0300

    Implementada a acao de vender produtos

--- src/com/pdv/dao/ProdutoDao.java
+++ src/com/pdv/dao/ProdutoDao.java
@@ -28,4 +28,69 @@ public class ProdutoDao {
 
 		return rs;
 	}
+
+	public String[] getDescPrecoById(Integer id) throws ClassNotFoundException, SQLException {
+		Connection con = ConexaoBd.conectar();
+
+		PreparedStatement stmt = con.prepareStatement("SELECT descricao, precounitario FROM Produto WHERE codprod = ?");
+		stmt.setInt(1, id);
+		ResultSet rs = stmt.executeQuery();
+
+		String[] vetorDados = new String[2];
+
+		while (rs.next()) {
+			vetorDados[0] = rs.getString("descricao");
+			vetorDados[1] = String.valueOf(rs.getDouble("precounitario"));
+		}
+
+		return vetorDados;
+
+	}
+
+	public Integer getQuantidadeById(Integer id) throws ClassNotFoundException, SQLException {
+		Connection con = ConexaoBd.conectar();
+
+		PreparedStatement stmt = con.prepareStatement("SELECT qtdestoque FROM Produto WHERE codprod = ?");
+		stmt.setInt(1, id);
+		ResultSet rs = stmt.executeQuery();
+
+		int quantidade = 0;
+		while (rs.next()) {
+			quantidade = rs.getInt("qtdestoque");
+		}
+		return quantidade;
+
+	}
+
+	public Integer updateQuantidadeById(Integer id, Integer quantidadeComprada)
+			throws ClassNotFoundException, SQLException {
+		Connection con = ConexaoBd.conectar();
+
+		Integer qtdAtual = getQuantidadeById(id);
+
+		PreparedStatement stmt = con.prepareStatement("UPDATE Produto SET qtdestoque = ? WHERE codprod = ?");
+
+		stmt.setInt(1, qtdAtual - quantidadeComprada);
+		stmt.setInt(2, id);
+
+		return stmt.executeUpdate();
+	}
+
+	public boolean checkVenda(Integer id, Integer quantidadeComprada) throws ClassNotFoundException, SQLException {
+		Integer qtdAtual = getQuantidadeById(id);
+
+		if (qtdAtual < quantidadeComprada) {
+			return false;
+		}
+		return true;
+	}
+
+	public String vender(Integer id, Integer quantidadeComprada) throws ClassNotFoundException, SQLException {
+		if (checkVenda(id, quantidadeComprada)) {
+			updateQuantidadeById(id, quantidadeComprada);
+			return "Produto adicionado!";
+		} else {
+			return "Produto sem estoque!";
+		}
+	}
 }
--- src/com/pdv/frame/Home.java
+++ src/com/pdv/frame/Home.java
@@ -10,6 +10,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -23,16 +24,26 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.border.BevelBorder;
+import javax.swing.JScrollPane;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+import javax.swing.event.AncestorListener;
+import javax.swing.event.AncestorEvent;
 
 public class Home {
 
 	private JFrame frame;
 	private JTextField txtDescricaoProduto;
-	private JTable table;
 	private JTextField txtTotalCompra;
 	private JComboBox<Integer> cbCliente = new JComboBox<Integer>();
 	private JComboBox<Integer> cbLocalidade = new JComboBox<Integer>();
 	private JComboBox<Integer> cbProduto = new JComboBox<Integer>();
+	private JSpinner spinnerQuantidade = new JSpinner();
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -77,114 +88,170 @@ public class Home {
 		frame.getContentPane().setLayout(null);
 
 		JLabel lblCaixaLivre = new JLabel("CAIXA LIVRE");
+		lblCaixaLivre.setBounds(10, 11, 402, 14);
 		lblCaixaLivre.setFont(new Font("Arial", Font.BOLD, 16));
 		lblCaixaLivre.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCaixaLivre.setForeground(Color.BLUE);
-		lblCaixaLivre.setBounds(10, 11, 402, 14);
 		frame.getContentPane().add(lblCaixaLivre);
 
 		JLabel lblCliente = new JLabel("Cliente Selecionado:");
+		lblCliente.setBounds(22, 70, 176, 14);
 		lblCliente.setFont(new Font("Arial", Font.BOLD, 12));
 		lblCliente.setForeground(new Color(0, 0, 128));
-		lblCliente.setBounds(22, 70, 176, 14);
 		frame.getContentPane().add(lblCliente);
-
 		cbCliente.setBounds(22, 95, 176, 20);
 		frame.getContentPane().add(cbCliente);
 
 		JLabel lblLocalVenda = new JLabel("Local de Venda:");
+		lblLocalVenda.setBounds(225, 70, 176, 14);
 		lblLocalVenda.setForeground(new Color(0, 0, 128));
 		lblLocalVenda.setFont(new Font("Arial", Font.BOLD, 12));
-		lblLocalVenda.setBounds(225, 70, 176, 14);
 		frame.getContentPane().add(lblLocalVenda);
-
 		cbLocalidade.setBounds(225, 95, 176, 20);
 		frame.getContentPane().add(cbLocalidade);
 
 		JLabel lblCodigoDoProduto = new JLabel("Codigo do Produto:");
+		lblCodigoDoProduto.setBounds(22, 126, 176, 14);
 		lblCodigoDoProduto.setForeground(new Color(0, 0, 128));
 		lblCodigoDoProduto.setFont(new Font("Arial", Font.BOLD, 12));
-		lblCodigoDoProduto.setBounds(22, 126, 176, 14);
 		frame.getContentPane().add(lblCodigoDoProduto);
-
-		/**
-		 * Evento para mudar a descricao sempre que selecionar outro produto
-		 */
+		cbProduto.setBounds(22, 151, 176, 20);
 		cbProduto.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent arg0) {
 				setTextDescricaoProduto();
 			}
 		});
-
-		cbProduto.setBounds(22, 151, 176, 20);
 		frame.getContentPane().add(cbProduto);
 
 		JLabel lblQuantidadeProduto = new JLabel("Quantidade:");
+		lblQuantidadeProduto.setBounds(225, 126, 176, 14);
 		lblQuantidadeProduto.setForeground(new Color(0, 0, 128));
 		lblQuantidadeProduto.setFont(new Font("Arial", Font.BOLD, 12));
-		lblQuantidadeProduto.setBounds(225, 126, 176, 14);
 		frame.getContentPane().add(lblQuantidadeProduto);
 
 		JLabel lblDescricaoProduto = new JLabel("Descricao do Produto:");
+		lblDescricaoProduto.setBounds(22, 182, 135, 14);
 		lblDescricaoProduto.setForeground(new Color(0, 0, 128));
 		lblDescricaoProduto.setFont(new Font("Arial", Font.BOLD, 12));
-		lblDescricaoProduto.setBounds(22, 182, 135, 14);
 		frame.getContentPane().add(lblDescricaoProduto);
 
 		txtDescricaoProduto = new JTextField();
+		txtDescricaoProduto.setBounds(22, 207, 379, 20);
 		txtDescricaoProduto.setFont(new Font("Tahoma", Font.BOLD, 13));
 		txtDescricaoProduto.setEditable(false);
 		txtDescricaoProduto.setHorizontalAlignment(SwingConstants.CENTER);
-		txtDescricaoProduto.setBounds(22, 207, 379, 20);
 		frame.getContentPane().add(txtDescricaoProduto);
 		txtDescricaoProduto.setColumns(10);
 
 		JButton btnVender = new JButton("Vender");
-		
+		btnVender.setBounds(151, 243, 138, 23);
+
 		/**
 		 * Evento para adicionar um item na tabela do form
 		 */
 		btnVender.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+
+				ProdutoDao dao = new ProdutoDao();
+
+				Integer id = (Integer) cbProduto.getSelectedItem();
+				Integer quantidadeComprada = (Integer) spinnerQuantidade.getValue();
+
+				try {
+					String aprovada = dao.vender(id, quantidadeComprada);
+
+					switch (aprovada) {
+					case "Produto adicionado!":
+						JOptionPane.showMessageDialog(null, "Produto adicionado!");
+
+						DefaultTableModel model = (DefaultTableModel) table.getModel();
+
+						model.addRow(new Object[] { dao.getDescPrecoById(id)[0], quantidadeComprada,
+								dao.getDescPrecoById(id)[1],
+								(Double.parseDouble(dao.getDescPrecoById(id)[1]) * quantidadeComprada) });
+
+						double valorTotal = 0;
+						for (int i = 0; i < table.getRowCount(); i++) {
+							double valor = (double) table.getValueAt(i, 3);
+							valorTotal = +valor;
+						}
+
+						if (txtTotalCompra.getText().isEmpty()) {
+							txtTotalCompra.setText(String.valueOf(valorTotal));
+						} else {
+							double valor = Double.parseDouble(txtTotalCompra.getText());
+							txtTotalCompra.setText(String.valueOf(valor+valorTotal));
+
+						}
+
+						break;
+
+					case "Produto sem estoque!":
+						JOptionPane.showMessageDialog(null, "Produto sem estoque");
+						break;
+					default:
+						JOptionPane.showMessageDialog(null, "SQL Errado");
+						break;
+					}
+				} catch (ClassNotFoundException | SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+		btnVender.setFont(new Font("Arial", Font.PLAIN, 11));
+		frame.getContentPane().add(btnVender);
+
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 				
 				
 			}
 		});
-		btnVender.setFont(new Font("Arial", Font.PLAIN, 11));
-		btnVender.setBounds(151, 243, 138, 23);
-		frame.getContentPane().add(btnVender);
-
-		JButton btnExcluir = new JButton("Excluir");
-		btnExcluir.setFont(new Font("Arial", Font.PLAIN, 11));
 		btnExcluir.setBounds(151, 414, 138, 23);
+		btnExcluir.setFont(new Font("Arial", Font.PLAIN, 11));
 		frame.getContentPane().add(btnExcluir);
 
-		table = new JTable();
-		table.setFont(new Font("Arial", Font.PLAIN, 12));
-		table.setBounds(10, 291, 402, 106);
-		frame.getContentPane().add(table);
+		JLabel lblNewLabel = new JLabel("Produto");
+		lblNewLabel.setBounds(10, 276, 71, 14);
+		frame.getContentPane().add(lblNewLabel);
 
 		JLabel lblTotalCompra = new JLabel("Total da Compra:");
+		lblTotalCompra.setBounds(165, 469, 101, 14);
 		lblTotalCompra.setForeground(new Color(0, 0, 128));
 		lblTotalCompra.setFont(new Font("Arial", Font.BOLD, 12));
-		lblTotalCompra.setBounds(165, 469, 101, 14);
 		frame.getContentPane().add(lblTotalCompra);
 
 		txtTotalCompra = new JTextField();
+		txtTotalCompra.setBounds(303, 467, 109, 20);
 		txtTotalCompra.setEditable(false);
 		txtTotalCompra.setFont(new Font("Arial", Font.PLAIN, 12));
-		txtTotalCompra.setBounds(303, 467, 109, 20);
 		frame.getContentPane().add(txtTotalCompra);
 		txtTotalCompra.setColumns(10);
 
 		JButton btnFecharCompra = new JButton("Fechar Compra");
 		btnFecharCompra.setBounds(10, 503, 402, 23);
 		frame.getContentPane().add(btnFecharCompra);
-		
-		JSpinner spinnerQuantidade = new JSpinner();
-		spinnerQuantidade.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
+
 		spinnerQuantidade.setBounds(225, 151, 176, 20);
+		spinnerQuantidade.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
 		frame.getContentPane().add(spinnerQuantidade);
+
+		JScrollPane scrollPane = new JScrollPane();
+
+		scrollPane.setBounds(10, 327, 402, 71);
+		frame.getContentPane().add(scrollPane);
+
+		table = new JTable();
+		table.setModel(new DefaultTableModel(new Object[][] {},
+				new String[] { "Produto", "Quantidade", "Pre\u00E7o Unit\u00E1rio", "Valor Total" }));
+		table.getColumnModel().getColumn(0).setPreferredWidth(199);
+		table.getColumnModel().getColumn(1).setPreferredWidth(111);
+		table.getColumnModel().getColumn(2).setPreferredWidth(120);
+		table.getColumnModel().getColumn(3).setPreferredWidth(134);
+		scrollPane.setViewportView(table);
 	}
 
 	private void populateClienteBox() throws SQLException, ClassNotFoundException {
@@ -232,6 +299,7 @@ public class Home {
 			while (rs.next()) {
 				txtDescricaoProduto.setText(rs.getString("descricao"));
 			}
+			rs.close();
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
